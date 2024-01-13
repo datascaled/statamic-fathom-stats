d370ef922c24df4d444553b8566b324c2d55cd4b
Ramon Egger <ramon@jarvis.home>
2024-01-13 16:42:07 +0100

    initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Datascaled\StatamicFathomStats;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
